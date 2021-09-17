519941a0b9b51a1721495928b509e6fd45f87ef2
Amanda Souza <nanda.hamandadesouza@gmail.com>
2021-09-17 18:48:19 -0400

    Listando categorias

--- categories/CategorisController.js
+++ categories/CategorisController.js
@@ -6,7 +6,8 @@ const slugify = require('slugify')
 router.get('/admin/categories/new', (req, res) =>{
     res.render("admin/categories/new")
 })
-router.post('/categories/save', (req, res) =>{
+
+router.post('/save', (req, res) =>{
     var title = req.body.title;
     if(title != undefined){
 
@@ -18,12 +19,16 @@ router.post('/categories/save', (req, res) =>{
         })
 
     }else{
-        res.redirect('/admin/categories/new')
+        res.redirect('admin/categories/new')
     }
 })
 
 router.get('/admin/categories', (req, res) =>{
-    res.render("admin/categories/index")
+    
+    Category.findAll().then(categories =>{
+        res.render('admin/categories/index', {categories: categories})
+    })
+    
 
 })
 module.exports = router;
