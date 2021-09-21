42a40d75cf1c12019842d78836c157d9a26a1d36
Amanda Souza <nanda.hamandadesouza@gmail.com>
2021-09-20 22:39:21 -0400

    Feat. Editando o Artigo

--- articles/articlesControllers.js
+++ articles/articlesControllers.js
@@ -58,4 +58,36 @@ router.post('/articles/delete', (req, res) => {
     }
 });
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect('/admin/categories')
+    }
+
+    Article.findByPk(id).then(articles => {
+        if(articles != undefined){
+            res.render('admin/articles/edit', {articles: articles})
+        }else{
+        res.redirect('/admin/articles')
+        }
+    }).catch(error => {
+        res.redirect('/admin/articles')
+    })
+})
+
+router.post('/articles/edit/save', (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.content
+
+    Article.update({title: title, slug: slugify(title), body: body}, {
+            where:{ 
+                id: id
+            }
+        }).then(() => {
+            res.redirect('/admin/articles')
+        })
+
+})
 module.exports = router
