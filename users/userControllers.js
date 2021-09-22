4dff45eab0246dea314d728e0dce99a58fdf8170
Amanda Souza <nanda.hamandadesouza@gmail.com>
2021-09-21 23:04:05 -0400

    Listagem de usuarios

--- users/userControllers.js
+++ users/userControllers.js
@@ -2,20 +2,50 @@ const express = require('express')
 const router = express.Router()
 const Category = require('../categories/Category')
 const User = require('./User')
+const bcrypt = require('bcryptjs');
+
 
 router.get('/admin/users', (req, res) => {
     res.send('Users')
 })
 
-router.get('/admin/users/create', (req, res) => {
-    res.render('admin/users/create')
+
+router.get('/admin/users/list', (req, res) => {
+    User.findAll().then(user => {
+        res.render('admin/users/list', {user: user})
+
+    })
 })
 
 router.post('/users/create', (req, res) => {
     var email = req.body.email;
     var password = req.body.password
 
-    res.json({email, password})
+
+    User.findOne({where:{email: email}}).then(user =>{
+        if(user == undefined){
+
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt)
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+            res.redirect('/')
+            }).catch((err) => {
+            res.redirect('/')
+            })
+
+        }else{
+            res.redirect('/admin/users/create')
+        }
+    })
+
+
+
+    
+
 })
 
 
